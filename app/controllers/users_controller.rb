b3431ac7a46deedf01b1483fc5170775c0249698
meniality <patrh_99@hotmail.com>
2020-04-20 13:27:16 -0600

    added a create method for user. removed merged user from strong params for now.

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,13 @@
 class UsersController < ApplicationController
 
+  def create
+    user = User.create(user_params)
+    render json: user
+  end
+
+  private 
+
   def user_params
-    params.require("user").permit(:username, :password, :email).merge(user: @user)
+    params.require("user").permit(:username, :password, :email)
   end
 end
